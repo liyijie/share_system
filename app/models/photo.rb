ff0f9dac9eed1fd7cdf22830268d06e03afcde3c
liyijie <liyijie825@gmail.com>
2014-09-02 19:02:53 +0800

    add image attribute to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,4 +9,10 @@
 #
 
 class Photo < ActiveRecord::Base
+  
+  has_attached_file :image, :styles => { :product => '320x320>'}
+  
+  validates_attachment_presence :image
+  validates_attachment_size     :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'application/octet-stream"']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902093831) do
+ActiveRecord::Schema.define(version: 20140902110231) do
 
   create_table "photos", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
